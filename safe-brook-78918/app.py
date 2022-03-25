dadfc8e57cc3338aa39d5ed0b86d1e5dba86c88b
saip-chatbot <99175575+saip-chatbot@users.noreply.github.com>
2022-03-26 02:48:04 +0300

    Update app.py

--- safe-brook-78918/app.py
+++ safe-brook-78918/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request,jsonify
 from chat import handle_message
+from chat import recommend
 from flask_cors import CORS, cross_origin
 
 
@@ -13,13 +14,16 @@ def index_get():
    return render_template("base.html")
 
 
+
 @app.post("/predict")
 @cross_origin()
 def predict():
-     text = request.get_json(force=True).get("message")
-     response = handle_message(text)
-     message = {"answer": response}
-     return jsonify(message)
+    text = request.get_json(force=True).get("message")
+    response = handle_message(text)
+    rec = recommend(response)
+    recAnswer = handle_message(rec)
+    message = {"answer": response , "recommend": rec, "rAnswer": recAnswer}
+    return jsonify(message)
 
 
 if __name__ == "__main__":
